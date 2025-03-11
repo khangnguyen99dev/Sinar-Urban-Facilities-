3eb16faef96486d28dfd4bffaefded2922a3b4a9
ChisThanh <nguyenvct@visglobalsolutions.com>
2025-03-11 14:38:32 +0700

    Backend - add api about-us - add api blog - add api company-info - add api impressive case study - add api portfolio - add api service - add api team - add api testimonial

---
+++ src/api/impressive-case-study/controllers/impressive-case-study.ts
@@ -0,0 +1,7 @@
+/**
+ * impressive-case-study controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::impressive-case-study.impressive-case-study');
---
+++ src/api/impressive-case-study/routes/impressive-case-study.ts
@@ -0,0 +1,7 @@
+/**
+ * impressive-case-study router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::impressive-case-study.impressive-case-study');
