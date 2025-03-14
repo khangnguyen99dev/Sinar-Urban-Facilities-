31b08c800372b1a02891877ff5961f3a4b0c831e
Khang <ankhangn@visglobalsolutions.com>
2025-03-14 16:37:32 +0700

    Fixing

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     api_url: 'https://faithful-song-bcd3d2424a.strapiapp.com/api',
-    site_url: 'https://faithful-song-bcd3d2424a.strapiapp.com',
+    site_url: '',
     api_token_identifier: 'APIToken@sinarurbanfacilities'
 };
