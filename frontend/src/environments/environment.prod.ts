f2599097e7017b340c66a82b3186cafb2638e7c9
Khang <ankhangn@visglobalsolutions.com>
2025-03-14 15:36:46 +0700

    remove frontend

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    api_url: 'https://faithful-song-bcd3d2424a.strapiapp.com/api',
-    site_url: 'https://faithful-song-bcd3d2424a.strapiapp.com',
-    api_token_identifier: 'APIToken@sinarurbanfacilities'
-};
